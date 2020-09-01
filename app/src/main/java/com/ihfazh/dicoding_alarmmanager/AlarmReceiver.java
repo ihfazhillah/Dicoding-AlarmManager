6a975800fc03538bc756e5f63b4ffbcaa4d44a4b
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-09-01 12:10:28 +0700

    set one time alarm manager

--- app/src/main/java/com/ihfazh/dicoding_alarmmanager/AlarmReceiver.java
+++ app/src/main/java/com/ihfazh/dicoding_alarmmanager/AlarmReceiver.java
@@ -1,9 +1,18 @@
 package com.ihfazh.dicoding_alarmmanager;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AlarmReceiver extends BroadcastReceiver {
     public static final String TYPE_ONE_TIME = "type one time";
@@ -28,5 +37,50 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     private void showToast(Context context, String title, String message) {
         Toast.makeText(context, title + " : " + message, Toast.LENGTH_LONG).show();
+    }
+
+    public void setOneTimeAlarmManager(Context context, String type, String date, String time, String message){
+        String dateFormat = "yyyy-MM-dd";
+        String timeFormat = "HH:mm";
+
+        if (dateInvalid(date, dateFormat) || dateInvalid(time, timeFormat)) return;
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_TYPE, type);
+
+        Log.e("ONE TIME", date + " " + time);
+        String[] dateArray = date.split("-");
+        String[] timeArray = time.split(":");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, Integer.parseInt(dateArray[0]));
+        calendar.set(Calendar.MONTH, Integer.parseInt(dateArray[1]) - 1);
+        calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateArray[2]));
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+        calendar.set(Calendar.SECOND, 0);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, ID_ONE_TIME, intent, 0);
+
+        if (alarmManager != null){
+            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        }
+
+        Toast.makeText(context, "One Time Alarm set", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private boolean dateInvalid(String time, String timeFormat) {
+        try {
+            DateFormat df = new SimpleDateFormat(timeFormat, Locale.getDefault());
+            df.setLenient(false);
+            df.parse(time);
+            return true;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
