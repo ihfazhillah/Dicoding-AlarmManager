f8b5169617724052cd9d3876375659e85cc0acf8
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-09-02 11:13:17 +0700

    implement click listener

--- app/src/main/java/com/ihfazh/dicoding_alarmmanager/MainActivity.java
+++ app/src/main/java/com/ihfazh/dicoding_alarmmanager/MainActivity.java
@@ -2,6 +2,7 @@ package com.ihfazh.dicoding_alarmmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener{
 
     TextView tvOnceDate, tvOnceTime;
     EditText etOnceMessage;
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnSetOnce;
 
     private AlarmReceiver receiver;
+
+    final String DATE_PICKER_TAG = "DatePicker";
+    final String TIME_PICKER_ONCE_TAG = "TimePickerOnce";
+    final String TIME_PICKER_REPEAT_TAG = "TimePickerRepeat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +47,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btn_once_date:
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(getSupportFragmentManager(), DATE_PICKER_TAG);
                 break;
             case R.id.btn_once_time:
+                TimePickerFragment timePickerFragmentOne = new TimePickerFragment();
+                timePickerFragmentOne.show(getSupportFragmentManager(), TIME_PICKER_ONCE_TAG);
                 break;
             case R.id.btn_set_once_button:
-                break;
+                String onceDate = tvOnceDate.getText().toString();
+                String onceTime = tvOnceTime.getText().toString();
+                String message = etOnceMessage.getText().toString();
+
+                receiver.setOneTimeAlarmManager(
+                        this,
+                        AlarmReceiver.TYPE_ONE_TIME,
+                        onceDate,
+                        onceTime,
+                        message
+                );
         }
+
+    }
+
+    @Override
+    public void onDialogDateSet(String tag, int year, int month, int dayOfMonth) {
+
+    }
+
+    @Override
+    public void onTimeSet(String tag, int hour, int minute) {
 
     }
 }
